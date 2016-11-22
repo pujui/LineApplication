038a187bb4142facec543db3cf2248d18d4d408c
Kim <kim.hung@uitox.com>
2016-11-22 15:52:49 +0800

    test

---
+++ LineApplication/protected/managers/TemplateMessageManager.php
@@ -0,0 +1,25 @@
+<?php
+class TemplateMessageManager{
+
+    public $parent = null;
+    
+    public function KILLER($userId, $list){
+        $messages = [];
+        $action = ['type' => 'message', 'label' => 'open', 'text' => '/kill'];
+        foreach ($list as $number=>$row){
+            if($row['status'] == $this->ROLE_STATUS['NORMAL']){
+                $action['label'] = sprintf('Player %d. %s', $number, $row['displayName']);
+                $action['text'] = sprintf('/kill %d', $number);
+                $messages[] = $action;
+            }
+        }
+        return [
+                    'type'      => 'template',
+                    'altText'   => '該樣板只能手機板看到',
+                    'template'  => [
+                        'type'  => 'buttons', 'title' => '虐殺名單', 'text'  => '選擇對象',
+                        'actions' => $messages
+                    ]
+                ];
+    }
+}
