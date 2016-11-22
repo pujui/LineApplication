bcf9b5436a605ed3ed8811821df57fa5fd4cd7b9
Kim <kim.hung@uitox.com>
2016-11-22 16:18:54 +0800

    test

--- LineApplication/protected/managers/TemplateMessageManager.php
+++ LineApplication/protected/managers/TemplateMessageManager.php
@@ -5,7 +5,7 @@ class TemplateMessageManager{
     
     public function KILLER($userId, $list){
         $messages = [];
-        $action = ['type' => 'message', 'label' => 'open', 'text' => '/kill'];
+        $action = ['type' => 'message', 'label' => '', 'text' => '/kill %d'];
         foreach ($list as $number=>$row){
             if($row['status'] == 'NORMAL'){
                 $action['label'] = sprintf('Player %d. %s', $number+1, $row['displayName']);
@@ -14,5 +14,31 @@ class TemplateMessageManager{
             }
         }
         $this->parent->actionPushTemplateButonMessages($userId, '虐殺名單-%d', '選擇對象', $messages);
+    }
+
+    public function PEEPER($userId, $list){
+        $messages = [];
+        $action = ['type' => 'message', 'label' => '', 'text' => '/peep %d'];
+        foreach ($list as $number=>$row){
+            if($row['status'] == 'NORMAL'){
+                $action['label'] = sprintf('Player %d. %s', $number+1, $row['displayName']);
+                $action['text'] = sprintf('/peep %d', $number+1);
+                $messages[] = $action;
+            }
+        }
+        $this->parent->actionPushTemplateButonMessages($userId, '偷窺名單-%d', '選擇對象', $messages);
+    }
+
+    public function HELPER($userId, $list){
+        $messages = [];
+        $action = ['type' => 'message', 'label' => '', 'text' => '/help %d'];
+        foreach ($list as $number=>$row){
+            if($row['status'] == 'NORMAL'){
+                $action['label'] = sprintf('Player %d. %s', $number+1, $row['displayName']);
+                $action['text'] = sprintf('/help %d', $number+1);
+                $messages[] = $action;
+            }
+        }
+        $this->parent->actionPushTemplateButonMessages($userId, '救援名單-%d', '選擇對象', $messages);
     }
 }
