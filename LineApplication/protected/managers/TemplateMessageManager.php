58270ba389ccba11259ec0584496d5d63c1b0ba7
Kim <kim.hung@uitox.com>
2016-11-22 16:11:17 +0800

    test

--- LineApplication/protected/managers/TemplateMessageManager.php
+++ LineApplication/protected/managers/TemplateMessageManager.php
@@ -7,13 +7,12 @@ class TemplateMessageManager{
         $messages = [];
         $action = ['type' => 'message', 'label' => 'open', 'text' => '/kill'];
         foreach ($list as $number=>$row){
-            if($row['status'] == $this->ROLE_STATUS['NORMAL']){
+            if($row['status'] == 'NORMAL'){
                 $action['label'] = sprintf('Player %d. %s', $number, $row['displayName']);
                 $action['text'] = sprintf('/kill %d', $number);
                 $messages[] = $action;
             }
         }
-        var_dump($messages);
         $this->parent->actionPushTemplateButonMessages($userId, '虐殺名單', '選擇對象', $messages);
     }
 }
